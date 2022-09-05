9f6bc80e253a17c711e996b227953be2662c4d48
ezesilva95 <4381@holbertonschool.com>
2022-09-04 21:03:40 -0700

    task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text variable
+(replace underscore _ symbols with a space )
+/python/(<text>): display “Python ”, followed by the value of the text variabl
+(replace underscore _ symb with space ) The default value of txt is “is cool”
+/number/<n>: display “n is a number” only if n is an integer
+/number_template/<n>: display a HTML page only if n is an integer:
+    h1 tag: “Number: n” inside the tag body
+"""
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+def hello_hbnb():
+    """
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    """
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def text_if_int(n):
+    """
+    """
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def html_if_int(n):
+    """
+    """
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
