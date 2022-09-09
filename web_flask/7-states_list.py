e1efef7033a8485e7d3b226bb2c9c19493cc6cc9
ezesilva95 <4381@holberton.students.com>
2022-09-09 01:55:32 +0000

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,84 +1,24 @@
 #!/usr/bin/python3
 """
-Write a script that starts a Flask web application:
-
-Your web application must be listening on 0.0.0.0, port 5000
-You must use storage for fetching data from the storage engine
-(FileStorage or DBStorage) => from models import storage and storage.all(...)
-After each request you must remove the current SQLAlchemy Session:
-Declare a method to handle @app.teardown_appcontext
-Call in this method storage.close()
-Routes:
-/states_list: display a HTML page: (inside the tag BODY)
-H1 tag: “States”
-UL tag: with the list of all State objects present in DBStorage sorted by name
-(A->Z) tip
-LI tag: description of one State: <state.id>: <B><state.name></B>
-Import this 7-dump to have some data
-You must use the option strict_slashes=False in your route definition
+task 8
 """
 
-from flask import Flask
+from flask import Flask, render_template
+from models import storage
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-def hello_hbnb():
-    """
-    """
-    return "Hello HBNB!"
+@app.teardown_appcontext
+def teardon_appcontext(self):
+    storage.close()
 
 
-@app.route('/hbnb')
-def hbnb():
-    """
-    """
-    return "HBNB"
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
 
-
-@app.route('/c/<text>')
-def c_text(text):
-    """
-    """
-    return "C {}".format(text.replace('_', ' '))
-
-
-@app.route('/python')
-@app.route('/python/<text>')
-def python_text(text="is cool"):
-    """
-    """
-    return "Python {}".format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>')
-def text_if_int(n):
-    """
-    """
-    return "{:d} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>')
-def html_if_int(n):
-    """
-    """
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>')
-def html_odd_or_even(n):
-    """
-    """
-    odd_or_even = "even" if (n % 2 == 0) else "odd"
-    return render_template('6-number_odd_or_even.html',
-                           n=n, odd_or_even=odd_or_even)
-
-
-def html_fetch_states():
-    """
-    """
-    state_objs = [s for s in storage.all("State").values()]
-    return render_template('7-states_list.html', state_objs=state_objs)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port='5000')
